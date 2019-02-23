effb786c0388f5a9d0f081fae50111493648be9d
Yingqiao Zheng <yingqiao.zoe@gmail.com>
2019-02-22 20:22:32 -0500

    finish hw 2 7

---
+++ HW2_7/src/com/ece/sad/Main.java
@@ -0,0 +1,9 @@
+package com.ece.sad;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ProcessBigData process = new BigDataProcessor();
+	    process.processBigData();
+    }
+}
